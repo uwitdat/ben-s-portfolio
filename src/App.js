3c5496498eb15997abb8dce992a28cc0e8304e23
Ben Shekhtman <benshekhtman@hotmail.com>
2021-06-19 16:34:57 -0400

    added contact me

--- src/App.js
+++ src/App.js
@@ -159,21 +159,31 @@ const App = () => {
         <div className="flx-row">
 
           <div className="flx-txt">
-            <p id="top">Based in</p>
-            <p className="pink" id="top-bold">benshekhtman</p>
+            <p id="top-shadow">Based in</p>
+            <div className="more"><p className="pink" id="top-bold">benshekhtman</p></div>
           </div>
 
           <div className="flx-txt-two">
-            <p id="top">Toronto, on</p>
-            <p className="pink" id="top-bold">@hotmail.com</p>
+            <p id="top-shadow-two">Toronto, on</p>
+            <div className="more-two"><p className="pink" id="top-bold-two">@hotmail.com</p></div>
           </div>
 
         </div>
 
         <div className="links">
-          <AiFillLinkedin id='link' />
-          <AiFillFacebook id='link' />
-          <DiGithubBadge id='link' />
+          <a href='https://github.com/uwitdat'
+            target='_blank'>
+            <AiFillLinkedin id='link'
+            />
+          </a>
+          <a href='https://www.facebook.com/ben.shekhtman/'
+            target='_blank'>
+            <AiFillFacebook id='link' />
+          </a>
+          <a href='https://www.linkedin.com/in/ben-shekhtman-751475178/'
+            target='_blank'>
+            <DiGithubBadge id='link' />
+          </a>
         </div>
       </div>
 
