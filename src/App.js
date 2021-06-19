b6f8d3ea1b44ce8073e864e77964b910005fd7fa
Ben Shekhtman <benshekhtman@hotmail.com>
2021-06-19 02:13:00 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
+import React, { useState } from 'react';
+import { SliderData } from './SliderData'
+import { SliderDataTwo } from './SliderData'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
 import './App.css';
 
 const App = () => {
+  const length = SliderData.length
+  const lengthTwo = SliderDataTwo.length
+
+  const [current, setCurrent] = useState(0)
+  const [currentTwo, setCurrentTwo] = useState(0)
+
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+
   return (
     <>
       <div className="App-container">
-        <video id="vid" src='myVid.mp4' loop="true" autoplay="autoplay" muted ></video>
+        <video id="vid" src='myVid.mp4' loop="true" autoplay="autoplay" muted />
         <div className="header">
           <h1 className="title">BEN SHEKHTMAN </h1>
         </div>
@@ -24,33 +43,51 @@ const App = () => {
       </div>
 
       <div className="container-two">
+        <h1 className="title">PLAYDOG</h1>
+        <div className='col-2-pics'>
+          {SliderData.map((slide, index) => {
+            return (
+              <div className={index === current ? 'slide active' : 'slide'}
+                key={index}
+              >
+                {index === current && (<img id='playdog' src={slide.image}></img>)}
+              </div>
+            )
+          })}
+        </div>
+
+        <div className="arrow">
+          <FaArrowAltCircleLeft className='right-arr' onClick={prevSlide} />
+          <FaArrowAltCircleRight className='right-arr' onClick={nextSlide} />
+        </div>
+      </div>
+
+
+
+
+      <div className="container-three">
         <div className="content">
           <p id='lan'>// LANGUAGES</p>
           <hr />
           <p id="txt">JAVASCRIPT, PYTHON, RUBY</p>
           <p id="txt"> HTML, CSS, SASS</p>
         </div>
-
-
         <div className="content">
           <p id='lan'>// FRAMEWORKS</p>
           <hr />
           <p id="txt">REACT, EXPRESS, NODE, DJANGO</p>
           <p id="txt">RUBY ON RAILS, BOOTSTRAP, MATERIALIZE</p>
         </div>
-
         <div className="content">
           <p id='lan'>// DATABASES</p>
           <hr />
           <p id="txt">MONGODB, PSQL, SQLITE</p>
         </div>
-
         <div className="content">
           <p id='lan'>// DEV-OPS</p>
           <hr />
           <p id="txt">GITHUB, AWS, HEROKU</p>
         </div>
-
       </div>
 
 
---
+++ src/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+    {
+        image: 'playdog.png'
+    },
+    {
+        image: 'playdog2.png'
+    },
+    {
+        image: 'playdog3.png'
+    },
+]
+
+export const SliderDataTwo = [
+    {
+        image: 'trader2.png'
+    },
+    {
+        image: 'trader1.png'
+    },
+    {
+        image: 'trader3.png'
+    },
+    {
+        image: 'trader4.png'
+    },
+]
